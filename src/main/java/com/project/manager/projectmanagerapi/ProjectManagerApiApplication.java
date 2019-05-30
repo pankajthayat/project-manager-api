ec2744f7c90b3662cefe4484c44d55b68392cd2b
pankajthayat <pankajthayat.com>
2019-05-30 09:19:55 +0530

    initial setup

---
+++ src/main/java/com/project/manager/projectmanagerapi/ProjectManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.manager.projectmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagerApiApplication.class, args);
+	}
+
+}
